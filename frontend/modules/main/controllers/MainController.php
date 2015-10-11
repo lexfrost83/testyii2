a72ab92537e7e3e4c1e43641263bfb8d75773ca9
aleksandr <lex@email.com.ua>
2015-10-11 18:38:13 +0300

    add form registration

--- frontend/modules/main/controllers/MainController.php
+++ frontend/modules/main/controllers/MainController.php
@@ -2,13 +2,25 @@
 namespace app\modules\main\controllers;
 
 use frontend\models\Image;
+use frontend\models\SignupForm;
 
 class MainController extends \yii\web\Controller
 {
+    public $layout = "inner";
 
     public function actionIndex()
     {
         return $this->render('index');
     }
 
+    public function actionRegister()
+    {
+        $model = new SignupForm();
+        if($model->load(\Yii::$app->request->post()))
+        {
+            print_r($model->getAttributes());
+            die;
+        }
+        return $this->render('register', ['model' => $model]);
+    }
 }
